f3dd76cb250a3156dc0319370bf38915a86cb423
iqfighter <iqfighter123@hotmail.com>
2016-04-10 15:10:18 +0800

    refactor.

--- TaxiBilling/TaxiBilling/TaxiBilling.cs
+++ TaxiBilling/TaxiBilling/TaxiBilling.cs
@@ -10,26 +10,48 @@ namespace TaxiBilling
     {
         private double _unitPrice = 0.8;
         private double _extraPercentage = 0.5;
-        private int _extraChargeStartDistance = 8;
+        private int _extraChargeDistance = 8;
         public int StartPrice { get; } = 6;
         public int StartDistance { get; } = 2;
         public double WaitingPrice { get; } = 0.25;
 
         public double Bill(double distance, int waiting = 0)
         {
-            double result = waiting * WaitingPrice;
-            
-            if (distance <= StartDistance)
-                return StartPrice + result;
+            double waitingFee = waiting * WaitingPrice;
+            double distanceFee;
 
-            if (distance >= _extraChargeStartDistance)
+            if (distance <= StartDistance)
             {
-                result += _unitPrice * distance + _unitPrice * _extraPercentage * (distance - _extraChargeStartDistance);
-                return Math.Round(result, 2);
+                distanceFee = StartDistanceCalc(distance);
+                return Math.Round(distanceFee + waitingFee, 2);
             }
 
-            result += distance*_unitPrice;
-            return Math.Round(result, 2);
+            if (distance >= _extraChargeDistance)
+            {
+                distanceFee = ExceedDistanceCalc(distance);
+            }
+            else
+            {
+                distanceFee = CommonCalc(distance);
+            }
+
+            return Math.Round(distanceFee + waitingFee, 2);
         }
+
+        private double CommonCalc(double distance)
+        {
+            return distance*_unitPrice;
+        }
+
+        private double ExceedDistanceCalc(double distance)
+        {
+            return _unitPrice * distance + _unitPrice * _extraPercentage * (distance - _extraChargeDistance);
+        }
+
+        private double StartDistanceCalc(double distance)
+        {
+            return StartPrice;
+        }
+
     }
 }
