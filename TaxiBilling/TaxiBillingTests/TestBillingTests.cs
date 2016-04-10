fec2d555650320377f54ddec694ef4c48ca17f8c
WANG Yu(InterACT) <YWang38@slb.com>
2016-04-10 13:28:22 +0800

    Initial Project.

---
+++ TaxiBilling/TaxiBillingTests/TestBillingTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaxiBillingTests
+{
+    [TestClass]
+    public class TestBillingTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
